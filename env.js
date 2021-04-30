2516a45a9748a64836ac1c657cc4f7755979e4df
azilx <63718977+azilx@users.noreply.github.com>
2021-04-30 23:29:01 +0100

    Update env.js

--- env.js
+++ env.js
@@ -1,3 +1,5 @@
 const API_URL =
-  "http://06e63bb667ce.ngrok.io/api/";
-  export {API_URL };
+  "http://a31aca1a4c2f.ngrok.io/api/";
+  const GOOGLE_API =
+  "AIzaSyB2Yno10-YTnLjjn_Vtk0V8cdcY5lC4plU";
+  export {API_URL,GOOGLE_API };
--- example-list.js
+++ example-list.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 import Location from "./examples/Locations/Location";
+import Map from "./examples/Map/Map";
 
 
 export const EXAMPLE_LIST = [
   
   {
     name: "Locations",
-    level: 11,
+    level: 1,
     component: <Location />,
+  },
+  {
+    name: "Map",
+    level: 2,
+    component: <Map />,
   },
   
   
