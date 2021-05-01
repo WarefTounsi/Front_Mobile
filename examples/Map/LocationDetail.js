10f44ea43e6fcf918c82af7fd57624332ec51c3d
WarefTounsi <waref.tounsi@esprit.tn>
2021-05-01 05:40:23 +0100

    Onboarding 2 + LocationDetail
    design v1.0.0.0.0.0

--- examples/Map/LocationDetail.js
+++ examples/Map/LocationDetail.js
@@ -10,11 +10,12 @@ import {
   Button,
   Modal,
   TouchableOpacity,
-  Dimensions 
+  Dimensions,
+  SafeAreaView,
 } from "react-native";
-import ImageViewer from 'react-native-image-zoom-viewer';
+import ImageViewer from "react-native-image-zoom-viewer";
 
-import { IconButton, Colors } from 'react-native-paper';
+import { IconButton, Colors } from "react-native-paper";
 import { Icon } from "react-native-elements";
 import ProgressBar from "react-native-progress/Bar";
 import MainHeader from "./components/MainHeader";
@@ -22,7 +23,7 @@ import MainHeader from "./components/MainHeader";
 import Config from "react-native-config";
 
 // import { BackgroundColor } from "../constants";
-const BackgroundColor="#559EDF";
+const BackgroundColor = "#559EDF";
 
 import { API_URL } from "../../env";
 
@@ -33,7 +34,6 @@ export default function LocationDetail({ navigation, route }) {
   const [cpProgress, setCpProgress] = useState(0);
   const [media, setMedia] = useState([]);
 
-
   const maxSTA = 400;
   const maxATK = 400;
   const maxDEF = 400;
@@ -43,94 +43,85 @@ export default function LocationDetail({ navigation, route }) {
   const { location = {} } = route.params;
   const [modalVisible, setModalVisible] = useState(false);
 
-  function getPictures(){
-
-
-    fetch(API_URL+"locations/"+location.id+"/media", {
-      "method": "GET",
+  function getPictures() {
+    fetch(API_URL + "locations/" + location.id + "/media", {
+      method: "GET",
       // "headers": {
       //   "x-rapidapi-host": "quotes15.p.rapidapi.com",
       //   "x-rapidapi-key": "yourapikey"
       // }
     })
-      .then(response => response.json())
-      .then(response => {
+      .then((response) => response.json())
+      .then((response) => {
         // newArr.push(response.Locations);
         setMedia(response.Media);
         // console.lcog(response.Media)
-
       })
-      .catch(err => {
+      .catch((err) => {
         setMedia(err);
-
       });
-}
-  function showSlider(item){
+  }
+  function showSlider(item) {
     console.log(item);
     setModalVisible(true);
-  };
-  const renderPictures = ({item,index}) => {
-    
-    return (
-      <View >
-                <TouchableOpacity onPress={() => showSlider(index)}>
-
-      <Image
-                  style={{width: 100, height: 100, margin: 5,
-                  }} 
-      source={{ uri: item.url }}
-        />      
-        <Modal
-          visible={modalVisible}
-          transparent={true}
-          onRequestClose={() => setModalVisible(false)}
-        >
-          <ImageViewer imageUrls={media} index={index} />
-        </Modal>
-        </TouchableOpacity>
-    </View>
-    );
   }
-  function fetchData(item){
-    try{
-      if (item=="themes"){
-        fetch(API_URL+"locations/"+location.id+"/themes", {
-          "method": "GET",
+  const renderPictures = ({ item, index }) => {
+    return (
+      <SafeAreaView>
+        <View>
+          <TouchableOpacity onPress={() => showSlider(index)}>
+            <Image
+              style={{ width: 100, height: 100, margin: 5 }}
+              source={{ uri: item.url }}
+            />
+            <Modal
+              visible={modalVisible}
+              transparent={true}
+              onRequestClose={() => setModalVisible(false)}
+            >
+              <ImageViewer imageUrls={media} index={index} />
+            </Modal>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  };
+  function fetchData(item) {
+    try {
+      if (item == "themes") {
+        fetch(API_URL + "locations/" + location.id + "/themes", {
+          method: "GET",
           // "headers": {
           //   "x-rapidapi-host": "quotes15.p.rapidapi.com",
           //   "x-rapidapi-key": "yourapikey"
           // }
         })
-          .then(response => response.json())
-          .then(response => {
+          .then((response) => response.json())
+          .then((response) => {
             // newArr.push(response.Locations);
             let data = response.Themes;
             // console.log(response)
-            for (let theme of data){
-              console.log("teeeest")
-              theme["url"]=theme["photo"];
+            for (let theme of data) {
+              console.log("teeeest");
+              theme["url"] = theme["photo"];
             }
             setMedia(data);
             // console.log(response)
-    
           })
-          .catch(err => {
+          .catch((err) => {
             setMedia(err);
-    
           });
-      }
-      else if (item == "intro"){
+      } else if (item == "intro") {
         getPictures();
-      }
-      else if (item == "stations"){
-        fetch(API_URL+"stations/"+location.id+"/media", {
-          "method": "GET",
+      } else if (item == "stations") {
+        fetch(API_URL + "stations/" + location.id + "/media", {
+          method: "GET",
         })
-          .then(response => response.json())
-          .then(response => {
+          .then((response) => response.json())
+          .then((response) => {
             // newArr.push(response.Locations);
-            console.log("jaw")
-            console.log(response)
+            console.log("jaw");
+            console.log(response);
             let data = response.Media;
             // console.log(response)
             // for (let theme of data){
@@ -138,17 +129,13 @@ export default function LocationDetail({ navigation, route }) {
             // }
             setMedia(data);
             // console.log(response)
-    
           })
-          .catch(err => {
+          .catch((err) => {
             // console.log(err)
             setMedia(err);
-    
           });
       }
-    }catch(err){
-
-    }
+    } catch (err) {}
   }
 
   useEffect(() => {
@@ -167,79 +154,100 @@ export default function LocationDetail({ navigation, route }) {
   }, []);
 
   return (
-    <View style={styles.container} >
+    <View style={styles.container}>
       <Image
-      style={StyleSheet.absoluteFillObject}
-      source={{ uri: location.photo }}
-    />
+        style={StyleSheet.absoluteFillObject}
+        source={{ uri: location.photo }}
+      />
       <MainHeader navigation={navigation} />
 
       <ScrollView style={{ flex: 1 }}>
         <View style={styles.content}>
-        <TouchableOpacity onPress={() => console.log({process})}  style={styles.closeButton}><Text name="times" size={20} color="#757575" > test</Text>
+          <TouchableOpacity
+            onPress={() => console.log({ process })}
+            style={styles.closeButton}
+          >
+            <View flexDirection="row">
+              <IconButton
+                icon="plus"
+                color={Colors.blue200}
+                style={{ marginRight: 180 }}
+                size={30}
+                // style={{alignItems:"left"}}
+                onPress={() => console.log("added")}
+              />
+
+              <IconButton
+                icon="check"
+                color={Colors.blue200}
+                size={30}
+                alignItems="right"
+                // style={{alignItems:"left"}}
+                onPress={() => console.log("liked")}
+              />
+            </View>
           </TouchableOpacity>
-          <IconButton
-    icon="heart"
-    color={Colors.red500}
-    size={30}
-
-    // style={{alignItems:"left"}}
-    onPress={() => console.log('liked')}
-  />
-  <IconButton
-    icon="check"
-    color={Colors.blue200}
-    size={30}
-
-    // style={{alignItems:"left"}}
-    onPress={() => console.log('liked')}
-  />
           <Image
-            style={{width: 200, height: 200, borderRadius: 200/ 2, flex:2}} 
-            
+            style={{ width: 200, height: 200, borderRadius: 200 / 2, flex: 2 }}
             placeholderStyle={{ backgroundColor: "transparent" }}
             PlaceholderContent={<ActivityIndicator />}
             source={{ uri: location.photo }}
-          
           />
+          <View flexDirection="row">
+            <IconButton
+              icon="heart"
+              color={Colors.red500}
+              style={{ marginRight: 180 }}
+              size={30}
+              // style={{alignItems:"left"}}
+              onPress={() => console.log("liked")}
+            />
 
-          <Text style={styles.locationName}>{location.title}  </Text>
-
-          
-
-          <View>
-            <Text style={styles.description}>
-              {location.description_title} 
+            <Text
+              style={{
+                backgroundColor: "lightgrey",
+                borderRadius: 30,
+                height: 40,
+              }}
+            >
+              20 Km
             </Text>
           </View>
 
+          <Text style={styles.locationName}>{location.title} </Text>
+
           <View>
-          <View style={styles.buttons}>
-          <Button  onPress={() => fetchData("intro")}
-  title="Intro"
-  color={BackgroundColor}
-  accessibilityLabel="Learn more about this purple button"
-/><Button  onPress={() => fetchData("themes")}
-  title="Themes"
-
-  color={BackgroundColor}
-  accessibilityLabel="Learn more about this purple button"
-/>
-<Button  onPress={() => fetchData("stations")}
-  title="Stations"
-
-  color={BackgroundColor}
-  accessibilityLabel="Learn more about this purple button"
-/>
-</View>
-          <FlatList
-        data={media}
-        style={{flex:1}}
-        renderItem={renderPictures}
-        numColumns={3}/>
-
-
+            <Text style={styles.description}>{location.description_title}</Text>
           </View>
+
+          {/* <View> */}
+          <View style={styles.buttons}>
+            <Button
+              onPress={() => fetchData("intro")}
+              title="Intro"
+              color={BackgroundColor}
+              accessibilityLabel="Learn more about this purple button"
+            />
+            <Button
+              onPress={() => fetchData("themes")}
+              title="Themes"
+              color={BackgroundColor}
+              accessibilityLabel="Learn more about this purple button"
+            />
+            <Button
+              onPress={() => fetchData("stations")}
+              title="Stations"
+              color={BackgroundColor}
+              accessibilityLabel="Learn more about this purple button"
+            />
+          </View>
+          <FlatList
+            data={media}
+            style={{ flex: 1 }}
+            renderItem={renderPictures}
+            numColumns={3}
+          />
+          {/* </View> */}
         </View>
       </ScrollView>
     </View>
@@ -250,13 +258,12 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#559EDF",
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttons: {
-    flexDirection:"row",
+    flexDirection: "row",
     alignSelf: "center",
-
   },
   button: {
     padding: 10,
@@ -270,8 +277,8 @@ const styles = StyleSheet.create({
     minHeight: 500,
     marginTop: 150,
     marginBottom: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   avatar: {
     position: "absolute",
@@ -299,9 +306,7 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10,
   },
-  item: {
-    
-  },
+  item: {},
   // media: {
   //   backgroundColor: '#6495ED',
   //   alignItems: 'center',
--- screens/OnBoardingScreen.js
+++ screens/OnBoardingScreen.js
@@ -1,31 +1,88 @@
 import React from "react";
-import { View, Text, Button, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 import Onboarding from "react-native-onboarding-swiper";
+import { Props } from "react-native-image-zoom-viewer/built/image-viewer.type";
+
+const Dots = ({ selected }) => {
+  let backgroundColor;
+  backgroundColor = selected ? "rgba(0,0,0,0.8)" : "rgba(0,0,0,0.3)";
+
+  return (
+    <View
+      style={{
+        width: 5,
+        height: 5,
+        marginHorizontal: 3,
+        backgroundColor,
+      }}
+    />
+  );
+};
+const Skip = ({ ...props }) => <Button title="Skip" color="#000000" />;
+const Next = ({ ...props }) => (
+  <Button title="Next" color="#000000" {...props} />
+);
+const Done = ({ ...props }) => (
+  <TouchableOpacity style={{ marginHorizontal: 10 }} {...props}>
+    <Text style={{ fontSize: 16 }}>Done</Text>
+  </TouchableOpacity>
+);
 
 const OnBoardingScreen = ({ navigation }) => {
   return (
     <Onboarding
+      // imageContainerStyles={{
+      //   width: 100,
+      //   height: 100,
+      //   resizeMode: "contain",
+      // }}
+      SkipButtonComponent={Skip}
+      NextButtonComponent={Next}
+      DoneButtonComponent={Done}
+      DotComponent={Dots}
       onSkip={() => navigation.replace("Login")}
       onDone={() => navigation.navigate("Login")}
       pages={[
         {
           backgroundColor: "#fff",
-          image: <Image source={require("../assets/Logo.png")} />,
-          title: "Onboarding 1",
-          subtitle: "Done with React Native Onboarding Swiper",
+          image: (
+            <Image
+              style={{ width: 300, height: 250 }}
+              source={require("../assets/Logo.png")}
+            />
+          ),
+          title: "Tunisicone 1",
+          subtitle: "Welcome to Tunisia",
         },
         {
           backgroundColor: "#fff",
-          image: <Image source={require("../assets/Aziz.png")} />,
-          title: "Onboarding 2",
-          subtitle: "Done with React Native Onboarding Swiper",
+          image: (
+            <Image
+              style={{ width: 300, height: 250 }}
+              source={require("../assets/Aziz.png")}
+            />
+          ),
+          title: "This is Aziz ",
+          subtitle: "He developped this app",
         },
         {
           backgroundColor: "#fff",
-          image: <Image source={require("../assets/Waref.jpg")} />,
-          title: "Onboarding 3",
-          subtitle: "Done with React Native Onboarding Swiper",
+          image: (
+            <Image
+              style={{ width: 300, height: 250 }}
+              source={require("../assets/Waref.jpg")}
+            />
+          ),
+          title: "And this is Waref ",
+          subtitle: "Wallah I helped",
         },
       ]}
     />
@@ -39,5 +96,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    // flex: 1,
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
   },
 });
