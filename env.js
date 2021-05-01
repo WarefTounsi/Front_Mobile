784ff422501f00e98d1055fb9382553b607c5566
azilx <63718977+azilx@users.noreply.github.com>
2021-05-01 03:07:36 +0100

    Fix Navigation Container

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
-const API_URL = "http://22d9ee46c105.ngrok.io/api/";
+const API_URL = "http://dba1dabd18d2.ngrok.io/api/";
 const GOOGLE_API = "AIzaSyB2Yno10-YTnLjjn_Vtk0V8cdcY5lC4plU";
 export { API_URL, GOOGLE_API };
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
-  Image,
   SafeAreaView,
   ScrollView,
   View,
   BackHandler,
-  Button,
 } from "react-native";
 import { Text, ListItem } from "react-native-elements";
 import Constants from "expo-constants";
 import { EXAMPLE_LIST } from "../example-list";
 
-const [exampleIndex, setExampleIndex] = React.useState(null);
+export default function LoginScreen() {
+  const [exampleIndex, setExampleIndex] = useState(null);
 
-const LoginScreen = () => {
+  // Handle when user press Hardware Back Button
+  useEffect(() => {
+    const backAction = () => {
+      // Go back to Example List
+      if (exampleIndex !== null) {
+        setExampleIndex(null);
+      } 
+      // Exit app if user currently in Example List
+      else {
+        BackHandler.exitApp();
+      }
+
+      return true;
+    };
+
+    // https://reactnative.dev/docs/backhandler
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, [exampleIndex]);
+
+  if (exampleIndex !== null) return EXAMPLE_LIST[exampleIndex].component;
+
   return (
     <SafeAreaView style={styles.container}>
       <Text h4 style={styles.heading}>
-        React Native Expo Examples
+      Tunisicon functionalities
       </Text>
 
       <ScrollView>
@@ -36,44 +60,8 @@ const LoginScreen = () => {
       </ScrollView>
     </SafeAreaView>
   );
-};
+}
 
-export default LoginScreen;
-useEffect(() => {
-  // AsyncStorage.getItem("alreadyLaunched").then((Value) => {
-  //   if (Value == null) {
-  //     AnsyncStorage.setItem("alreadyLaunched", "true");
-  //     setIsFirstLaunch(true);
-  //   } else {
-  //     setIsFirstLaunch(false);
-  //   }
-  // });
-
-  const backAction = () => {
-    // Go back to Example List
-    if (exampleIndex !== null) {
-      setExampleIndex(null);
-    }
-    // Exit app if user currently in Example List
-    else {
-      BackHandler.exitApp();
-    }
-
-    return true;
-  };
-
-  // https://reactnative.dev/docs/backhandler
-  const backHandler = BackHandler.addEventListener(
-    "hardwareBackPress",
-    backAction
-  );
-
-  return () => backHandler.remove();
-}, [exampleIndex]);
-
-if (exampleIndex !== null) return EXAMPLE_LIST[exampleIndex].component;
-
-const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
